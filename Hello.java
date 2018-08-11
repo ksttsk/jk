599292350df0aa75514ca352c9df8b9d4accce8d
Wang <Haidong.Wang@otto.de>
2018-08-11 14:36:24 +0200

    change text

--- Hello.java
+++ Hello.java
@@ -1,9 +1,13 @@
 public class Hello {
 	
 	public static void main(String[] args) {
+		System.out.println("--------------");
+		
 		for(int i=1;i<=10;i++) {
 			System.out.println("Say Hello! " + i);
 			
 		}
+		
+		System.out.println("--------------");
 	}
 }
